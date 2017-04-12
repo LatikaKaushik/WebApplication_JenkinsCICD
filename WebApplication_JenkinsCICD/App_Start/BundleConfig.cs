ed88dc536eebaa53765170bd6bb5db15f6b361b3
latika <latika.kaushik@tridentinfo.com>
2017-04-12 12:25:47 +0530

    github commit

--- WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
+++ WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
@@ -12,7 +12,7 @@ namespace WebApplication_JenkinsCICD
                bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+              bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
