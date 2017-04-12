1a06cc5e7aa5294ddf0f943e8a65abb1a325d9b0
latika <latika.kaushik@tridentinfo.com>
2017-04-12 17:18:23 +0530

    next commit

--- WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
+++ WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebApplication_JenkinsCICD
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
