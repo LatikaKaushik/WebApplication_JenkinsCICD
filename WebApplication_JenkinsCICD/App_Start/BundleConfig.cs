f2979b31486ad53c511041872b7067d4070b5634
latika <latika.kaushik@tridentinfo.com>
2017-04-12 12:52:23 +0530

    next commit

--- WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
+++ WebApplication_JenkinsCICD/App_Start/BundleConfig.cs
@@ -17,7 +17,7 @@ namespace WebApplication_JenkinsCICD
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+              bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
              bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
